88ceecd926bd582b724e5cef3773acc84ad4cdd7
Marchalek <marchalek@hotmail.com>
2021-07-17 16:49:29 -0300

    Curso 1 terminado

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,15 +1,15 @@
 class NegociacaoController {
-  private _inputData: HTMLInputElement;
-  private _inputQuantidade: HTMLInputElement;
-  private _inputValor: HTMLInputElement;
+  private _inputData: JQuery;
+  private _inputQuantidade: JQuery;
+  private _inputValor: JQuery;
   private _negociacoes = new Negociacoes();
   private _negocieacoesView = new NegociacoesView("#negociacoesView");
   private _mensagemView = new MensagemView('#mensagemView')
 
   constructor() {
-    this._inputData = document.querySelector("#data");
-    this._inputQuantidade = document.querySelector("#quantidade");
-    this._inputValor = document.querySelector("#valor");
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
     this._negocieacoesView.update(this._negociacoes);
   }
 
@@ -17,9 +17,9 @@ class NegociacaoController {
     event.preventDefault();
 
     const negociacao = new Negociacao(
-      new Date(this._inputData.value.replace(/-/, ",")),
-      parseInt(this._inputQuantidade.value),
-      parseFloat(this._inputValor.value)
+      new Date(this._inputData.val().replace(/-/, ",")),
+      parseInt(this._inputQuantidade.val()),
+      parseFloat(this._inputValor.val())
     );
 
     this._negociacoes.adiciona(negociacao);
